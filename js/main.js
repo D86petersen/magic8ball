738590a9cedfc3b84746603eb2fe226591b7df86
Dustin Petersen <d86petersen@gmail.com>
2022-03-23 22:45:13 -0700

    added 8ball images and fixed a few bugs

--- js/main.js
+++ js/main.js
@@ -1,52 +1,49 @@
+// selecting button and assigning it to a variable
 let shakeButton = document.querySelector('#btn')
 
-
+// created addEventListener and function with an array of images
 shakeButton.addEventListener('click', function ask(){
     let answers = [
-        'As I see it, yes.',
-        'Ask again later.',
-        'Better not tell you now.',
-        'Cannot predict now.',
-        'Concentrate and ask again.',
-        'Don’t count on it.',
-        'It is certain.',
-        'It is decidedly so.',
-        'Most likely.',
-        'My reply is no.',
-        'My sources say no.',
-        'Outlook not so good.',
-        'Outlook good.',
-        'Reply hazy, try again.',
-        'Signs point to yes.',
-        'Very doubtful.',
-        'Without a doubt.',
-        'Yes.',
-        'Yes – definitely.',
-        'You may rely on it.'
+        '/img/magic8ball_1.png',
+        '/img/magic8ball_2.png',
+        '/img/magic8ball_3.png',
+        '/img/magic8ball_4.png',
+        '/img/magic8ball_5.png',
+        '/img/magic8ball_6.png',
+        '/img/magic8ball_7.png',
+        '/img/magic8ball_8.png',
+        '/img/magic8ball_9.png',
+        '/img/magic8ball_10.png',
+        '/img/magic8ball_11.png',
+        '/img/magic8ball_12.png',
+        '/img/magic8ball_13.png',
+        '/img/magic8ball_14.png',
+        '/img/magic8ball_15.png',
+        '/img/magic8ball_16.png',
+        '/img/magic8ball_17.png',
+        '/img/magic8ball_18.png',
+        '/img/magic8ball_19.png',
+        '/img/magic8ball_20.png',
     ];
-    //getting random number and matching it to an answer then outputting into an html element
-    let number = Math.floor(Math.random() * 20);
-    document.getElementById('answers').innerHTML = answers[number];
-    let answer = document.querySelector('#answers')
-answer.style.display = 'block';
-setTimeout(() => {
-  answersInput.style.display = "none";
-}, 5000);
+    //getting random number and matching it to an answer/pic then outputting into an html element
+    let number = Math.floor(Math.random() * 19);
+    // setting a delay in picture change
+    setTimeout(() => {
+    document.getElementById('eightball').src = answers[number];
+    }, 1000); 
+  
 })
-
+// created a function to make magic 8ball shake
 function anime() {
-  // Searches for Button and Click Event
+  // Searches for Button and Click Event and shakes 8ball
   document.querySelector('#btn').addEventListener('click', () => {
       document.querySelector('#eightball').classList.add("animate");
+      // sets removes shaking animation after 2 seconds
       setTimeout(() => {
           document.querySelector('#eightball').classList.remove("animate");
         }, 2000);
   })
-  let answer = document.querySelector('#answers')
-  answer.style.display = 'block';
-  setTimeout(() => {
-    answer.style.display = "none";
-  }, 3000);
+  // user question input removed on button click for 3 seconds then replaced 
   let answersInput = document.getElementById('div3');
   answersInput.style.display = "none";
   setTimeout(() => {
